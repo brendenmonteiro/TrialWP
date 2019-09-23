be6dacee086198f9666f97e8e151857b9a84e675
Gaurav Gosain <gg68@hw.ac.uk>
2019-09-23 08:50:10 +0400

    Summary

---
+++ trial.java
@@ -0,0 +1,17 @@
+
+public class trial {
+public static void main(String[]arr){
+	System.out.println("Hello");
+	System.out.println("World");
+	System.out.println("Hello GitHub");
+	System.out.println(1+1);
+	System.out.println(1+2);
+	System.out.println("Hi Vishan");
+  	int a = 2+4;
+	System.out.println(a);
+	System.out.println("Just wanted to see if this shite works");
+	System.out.println("lol");
+	System.out.println("i just wanna enjoy some death");
+	System.out.println("I HOPE THIS WORKS ");
+}
+}
